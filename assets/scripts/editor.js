3f01b37a8f5a39d9baa086462654035a2572d114
francois <francois.piquard11@gmail.com>
2024-12-20 02:32:38 +0000

    Working but unstyled editor

--- assets/scripts/editor.js
+++ assets/scripts/editor.js
@@ -1,15 +1,288 @@
-function restartAnimation(iframe) {
-  if (!iframe) return;
+let currentStyleContent = "";
+let isAdvanced = true;
 
+const allowedProperties = {
+  basic: [
+    {
+      property: "background-color",
+      label: "Background Color",
+      type: "color",
+      category: "appearance",
+    },
+    {
+      property: "color",
+      label: "Text Color",
+      type: "color",
+      category: "appearance",
+    },
+    {
+      property: "width",
+      label: "Width",
+      type: "text",
+      category: "size",
+    },
+    {
+      property: "height",
+      label: "Height",
+      type: "text",
+      category: "size",
+    },
+    {
+      property: "border-radius",
+      label: "Border Radius",
+      type: "text",
+      category: "appearance",
+    },
+  ],
+  advanced: [
+    {
+      property: "border-color",
+      label: "Border Color",
+      type: "color",
+      category: "border",
+    },
+    {
+      property: "border-width",
+      label: "Border Width",
+      type: "text",
+      category: "border",
+    },
+    {
+      property: "margin",
+      label: "Margin",
+      type: "text",
+      category: "spacing",
+    },
+    {
+      property: "padding",
+      label: "Padding",
+      type: "text",
+      category: "spacing",
+    },
+    {
+      property: "font-size",
+      label: "Font Size",
+      type: "text",
+      category: "typography",
+    },
+    {
+      property: "font-weight",
+      label: "Font Weight",
+      type: "text",
+      category: "typography",
+    },
+    {
+      property: "text-align",
+      label: "Text Align",
+      type: "select",
+      options: ["left", "center", "right"],
+      category: "typography",
+    },
+    {
+      property: "text-transform",
+      label: "Text Transform",
+      type: "select",
+      options: ["none", "uppercase", "lowercase", "capitalize"],
+      category: "typography",
+    },
+    {
+      property: "animation-duration",
+      label: "Animation Duration",
+      type: "text",
+      category: "animation",
+    },
+    {
+      property: "animation-delay",
+      label: "Animation Delay",
+      type: "text",
+      category: "animation",
+    },
+    {
+      property: "animation-iteration-count",
+      label: "Animation Repeat",
+      type: "text",
+      category: "animation",
+    },
+    {
+      property: "animation-timing-function",
+      label: "Animation Timing",
+      type: "select",
+      options: ["linear", "ease", "ease-in", "ease-out", "ease-in-out"],
+      category: "animation",
+    },
+    {
+      property: "animation-direction",
+      label: "Animation Direction",
+      type: "select",
+      options: ["normal", "reverse", "alternate", "alternate-reverse"],
+      category: "animation",
+    },
+    {
+      property: "animation-fill-mode",
+      label: "Animation Fill Mode",
+      type: "select",
+      options: ["none", "forwards", "backwards", "both"],
+      category: "animation",
+    },
+    {
+      property: "stroke-dasharray",
+      label: "Stroke Dash Array",
+      type: "text",
+      category: "svg",
+    },
+    {
+      property: "stroke-dashoffset",
+      label: "Stroke Dash Offset",
+      type: "text",
+      category: "svg",
+    },
+    {
+      property: "stroke-width",
+      label: "Stroke Width",
+      type: "text",
+      category: "svg",
+    },
+    {
+      property: "stroke",
+      label: "Stroke Color",
+      type: "color",
+      category: "svg",
+    },
+    {
+      property: "fill",
+      label: "Fill Color",
+      type: "color",
+      category: "svg",
+    },
+    {
+      property: "stroke-linecap",
+      label: "Stroke Line Cap",
+      type: "select",
+      options: ["butt", "round", "square"],
+      category: "svg",
+    },
+  ],
+};
+
+function createInputField(ruleSelector, property, value) {
+  const propertyInfo = [
+    ...allowedProperties.basic,
+    ...(isAdvanced ? allowedProperties.advanced : []),
+  ].find((item) => item.property === property);
+
+  if (!propertyInfo) return null;
+
+  const childDiv = document.createElement("div");
+  childDiv.className = "flex items-center justify-between w-full";
+
+  const label = document.createElement("label");
+  label.textContent = propertyInfo.label + ": " + value;
+  label.style.setProperty("min-width", "100px");
+  label.className = "w-1/3 text-md font-bold";
+
+  let input;
+
+  if (propertyInfo.type === "select") {
+    input = document.createElement("select");
+    propertyInfo.options.forEach((option) => {
+      const optionElement = document.createElement("option");
+      optionElement.value = option;
+      optionElement.textContent = option;
+      optionElement.selected = option === value;
+      input.appendChild(optionElement);
+    });
+  } else if (propertyInfo.type === "color") {
+    input = document.createElement("input");
+    input.type = "color";
+    input.value = value;
+    input.className =
+      "p-1 h-10 w-14 block bg-white border border-gray-200 cursor-pointer rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700";
+  } else {
+    input = document.createElement("input");
+    input.type = propertyInfo.type;
+    input.className = "h-8 w-auto min-w-44";
+  }
+  if (input.type !== "select") {
+    input.value = value;
+  }
+  input.attributes.property = property;
+  input.attributes.rule_selector = ruleSelector;
+
+  childDiv.appendChild(label);
+  childDiv.appendChild(input);
+
+  return childDiv;
+}
+
+function generateOptions(currentStyleContent) {
+  const optionsContainer = document.getElementById("elegant-options-container");
+  if (!optionsContainer) return;
+
+  const css = new CSSStyleSheet();
+  css.replaceSync(currentStyleContent);
+  optionsContainer.innerHTML = "";
+  // Create input fields based on CSS properties
+  Array.from(css.cssRules).forEach((rule) => {
+    let parentDiv = document.createElement("div");
+    parentDiv.className = "flex items-start flex-col justify-between w-full";
+    const sectionTitle = document.createElement("h2");
+    sectionTitle.textContent = rule.selectorText.split("-")[2] || "Default";
+    sectionTitle.className = "text-2xl font-bold";
+    parentDiv.appendChild(sectionTitle);
+
+    Array.from(rule.style).forEach((property) => {
+      const inputField = createInputField(
+        rule.selectorText,
+        property,
+        rule.style.getPropertyValue(property)
+      );
+      if (inputField) {
+        parentDiv.appendChild(inputField);
+      }
+    });
+    optionsContainer.appendChild(parentDiv);
+  });
+}
+
+// Helper functions for color conversion
+function rgbToHex(rgb) {
+  if (!rgb) return null;
+  const rgbArr = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)/i);
+  return rgbArr
+    ? "#" +
+        ("0" + parseInt(rgbArr[1], 10).toString(16)).slice(-2) +
+        ("0" + parseInt(rgbArr[2], 10).toString(16)).slice(-2) +
+        ("0" + parseInt(rgbArr[3], 10).toString(16)).slice(-2)
+    : null;
+}
+
+function hexToRgba(hex, alpha) {
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
+function getInputTypeForProperty(property) {
+  if (property.includes("color")) return "color";
+  if (property.includes("background")) return "color";
+  if (property.includes("border")) return "color";
+  if (property.includes("width")) return "text";
+  if (property.includes("height")) return "text";
+  if (property.includes("margin")) return "text";
+  if (property.includes("padding")) return "text";
+  if (property.includes("border-radius")) return "text";
+
+  return "text";
+}
+
+function restartIframe(iframe) {
+  if (!iframe) return;
   try {
     // Get the iframe document
     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-
-    // Store the current content
-    const currentStyleContent =
-      iframeDoc.querySelector("style")?.innerHTML || "";
     const bodyContent = iframeDoc.body.innerHTML;
-
+    console.log(currentStyleContent);
     // Recreate the document content
     iframeDoc.open();
     iframeDoc.write(`
@@ -41,69 +314,82 @@ function restartAnimation(iframe) {
   }
 }
 
-let lastChange = null;
-function handleChange() {
-  let iframe = document.getElementById("previewFrame");
-  let styleContent = iframe.contentDocument.querySelector("style")?.innerHTML;
-  console.log("handleChange");
-  lastChange = lastChange || 0;
-  const now = Date.now();
-  if (now - lastChange > 1000) {
-    parseOptionsAndModifyStyle(styleContent, iframe);
-    restartAnimation(iframe);
-  }
-  lastChange = now;
-}
-
-function parseOptionsAndModifyStyle() {
-  let iframe = document.getElementById("previewFrame");
-  let styleContent = iframe.contentDocument.querySelector("style")?.innerHTML;
-  try {
-    if (!styleContent) return;
-
-    // Create a new stylesheet object
-    const tempStyle = document.createElement("style");
-    tempStyle.textContent = styleContent;
-
-    // Parse and modify the style content
-    const css = new CSSStyleSheet();
-    css.replaceSync(tempStyle.textContent);
-
-    // Modify the first rule (for demonstration purposes)
-    const style = css.cssRules[0];
-    style.style.setProperty("background-color", "red");
-
-    // Update the global styleContent variable
-    styleContent = Array.from(css.cssRules)
-      .map((rule) => rule.cssText)
-      .join("\n");
-
-    // Apply the updated style back to the iframe
-    const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
-    const iframeStyle = iframeDoc.querySelector("style");
-    if (iframeStyle) {
-      iframeStyle.innerHTML = styleContent;
-    }
-  } catch (error) {
-    console.error("Error modifying style:", error);
-  }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  currentStyleContent =
+    document
+      .getElementById("previewFrame")
+      .contentDocument.querySelector("style")?.innerHTML || "";
+  generateOptions(currentStyleContent);
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   const iframe = document.getElementById("previewFrame");
-  const restartAnimationButton = document.getElementById(
+  const restartIframeButton = document.getElementById(
     "restart-animation-button"
   );
-
-  let styleContent = "";
 
   if (iframe) {
     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
     styleContent = iframeDoc.querySelector("style")?.innerHTML || "";
   }
 
-  restartAnimationButton?.addEventListener("click", function () {
-    parseOptionsAndModifyStyle(styleContent, iframe);
-    restartAnimation(iframe);
+  restartIframeButton?.addEventListener("click", function () {
+    restartIframe(iframe);
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const saveButton = document.getElementById("save-button");
+  saveButton?.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("save");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const inputFields = document.querySelectorAll("input");
+  inputFields.forEach((input) => {
+    input.addEventListener("change", (e) => {
+      const iframe = document.getElementById("previewFrame");
+      const doc = iframe.contentDocument || iframe.contentWindow.document;
+      // Get the current style content
+      const currentStyleUnparsed = doc.querySelector("style")?.innerHTML || "";
+      const currentStyle = new CSSStyleSheet();
+      currentStyle.replaceSync(currentStyleUnparsed);
+
+      // Create new CSS content
+      const newRules = Array.from(currentStyle.cssRules)
+        .map((rule) => {
+          if (rule.selectorText === e.target.attributes.rule_selector) {
+            if (!rule.style) {
+              rule.style = document.createElement("div").style;
+            }
+            rule.style.setProperty(
+              e.target.attributes.property,
+              e.target.value
+            );
+          }
+          return rule.cssText;
+        })
+        .join("\n");
+
+      // Update both the iframe and current style content
+      currentStyleContent = newRules;
+
+      restartIframe(iframe);
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const advancedModeToggle = document.getElementById("advanced-mode-toggle");
+  advancedModeToggle?.addEventListener("change", function () {
+    isAdvanced = !isAdvanced;
+    generateOptions(currentStyleContent);
+  });
+});
+
+function resetLoader(style) {
+  currentStyleContent = style;
+  generateOptions(currentStyleContent);
+}
--- includes/admin-editor.php
+++ includes/admin-editor.php
@@ -2,7 +2,8 @@
 flex flex-col lg:flex-row-reverse">
     <!-- Preview Column -->
     <div class="w-full lg:w-2/3 md:p-2 lg:p-8 flex justify-center items-start">
-        <div class="resize relative w-full max-h-[80%] md:h-96 lg:h-[800px] max-w-7xl bg-white border-primary-light border-solid border-l-4 rounded-sm overflow-hidden">
+        <div class="resize relative w-full min-h-[400px]
+         max-h-[80%] md:h-96 lg:h-[800px] max-w-7xl bg-white border-primary-light border-solid border-l-4 rounded-sm overflow-hidden">
             <?php
             $path = plugin_dir_path(__FILE__) . '../serving/elegant-loader.php';
             if (file_exists($path)) {
@@ -10,7 +11,7 @@ flex flex-col lg:flex-row-reverse">
                 if (!empty($svg_url) && !empty(get_option('elegant_loader_style'))) {
             ?>
                     <iframe
-                        id="previewFrame" style="width:100%; height:100%;">
+                        id="previewFrame" style="width:100%; height:100%; min-height:400px;">
                     </iframe>
                     <script>
                         document.addEventListener('DOMContentLoaded', function() {
@@ -51,39 +52,32 @@ flex flex-col lg:flex-row-reverse">
     </div>
 
     <!-- Controls Column -->
-    <form onChange="handleChange()" id="elegant-options" class="w-full lg:w-1/3 p-4 py-8 flex flex-col justify-start items-start">
-        <h1 class="text-4xl font-black text-primary">Elegant Loader</h1>
-        <p class="text-lg font-bold text-almost-black mb-4 border-b-4 border-primary-light pb-4">Customize your elegant loader to fit your brand</p>
-        <div class="flex flex-col gap-4 p-4 w-full h-full ">
-            <div class="flex items-center justify-between w-full">
-                <label>Background Color</label>
-                <input type="color" class="h-8 w-16" value="#000000" />
-            </div>
+    <form id="elegant-options" class="w-full lg:w-1/3 p-4 py-8 flex flex-col justify-start items-start">
+        <div class="flex flex-row justify-between items-end gap-4 mb-4">
+            <h1 class="text-4xl font-black text-primary">Elegant Loader</h1>
+            <!-- Advanced Mode -->
+            <label class="inline-flex items-center cursor-pointer">
+                <input type="checkbox" value="" class="sr-only peer" id="advanced-mode-toggle">
+                <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary-light dark:peer-focus:ring-primary rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-primary-light"></div>
+                <span class="ms-3 text-sm font-medium text-primary">Advanced Mode</span>
+            </label>
+        </div>
+        <p class="text-lg font-bold text-almost-black  border-b-4 border-primary-light pb-4">Customize your elegant loader to fit your brand</p>
+        <!-- Editor -->
+        <div id="elegant-options-container" class="flex flex-col gap-4 w-full h-auto">
+        </div>
 
-            <div class="flex items-center justify-between w-full">
-                <label>Logo Size</label>
-                <input type="color" class="h-8 w-16" value="#000000" />
-            </div>
+        <div class="flex flex-row justify-between items-center gap-4 mt-4 border-t-4 border-primary-light pt-4 w-full">
+            <button id="save-button" class="bg-primary text-white px-4 py-2 rounded-sm flex items-center justify-center">
+                Save the loader
+            </button>
 
-            <div class="flex items-center justify-between w-full">
-                <label>Animation Speed</label>
-                <input type="range" min="0" max="100" value="50" class="w-32" />
-            </div>
-
-            <div class="flex items-center justify-between w-full">
-                <label>Fade Duration</label>
-                <input type="range" min="0" max="100" value="50" class="w-32" />
-            </div>
-
-            <div class="flex items-center justify-between w-full">
-                <label>Border Width</label>
-                <input type="range" min="0" max="100" value="50" class="w-32" />
-            </div>
-
-            <div class="flex items-center justify-between w-full">
-                <label>Shadow Opacity</label>
-                <input type="color" class="h-8 w-16" value="#999999" />
-            </div>
+            <button onClick="resetLoader(<?php echo get_plugin_files(__FILE__) . '../template/' . get_option('elegant_loader_style') . '.css'; ?>)"
+                class="bg-light-red text-white px-4 py-2 rounded-sm flex items-center justify-center">
+                Reset the loader
+            </button>
         </div>
     </form>
+
+
 </div>
